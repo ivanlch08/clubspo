6732f347aa22d011a7b7ad119603cea617d5732c
DESKTOP-LPOB9EE\Leonardo <ivanlch08@gmail.com>
2018-03-24 11:05:06 -0500

    se agrega nueva pagina para confirmar registro

---
+++ src/pages/confir-registro/confir-registro.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+//import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+
+/**
+ * Generated class for the ConfirRegistroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+//@IonicPage()
+@Component({
+  selector: 'page-confir-registro',
+  templateUrl: 'confir-registro.html',
+})
+export class ConfirRegistroPage {
+
+  //constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
+  }
+
+  /*ionViewDidLoad() {
+    console.log('ionViewDidLoad ConfirRegistroPage');
+  }*/
+
+}
