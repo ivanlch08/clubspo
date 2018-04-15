d6b0b3008eeafdb7d885d4f703a8c1846d93befd
DESKTOP-LPOB9EE\Leonardo <ivanlch08@gmail.com>
2018-04-14 23:21:17 -0500

    primera prueba con login a facebook desde app 08

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -68,13 +68,18 @@ export class HomePage {
   }//loginFaceWeb
 
   loginFaceApp(){
+    var mensaje = '';
     this.firebaseService.registrarLog('login App.. 00');
     this.facebook.login(['email','public_profile']).then((response:FacebookLoginResponse) => {
       this.firebaseService.registrarLog('login App.. 01');
       this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
         this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] };
       })
+    }).catch(function(error){
+      mensaje += 'login App.. ERROR;';
+      //mensaje += JSON.stringify(error);
     });
+    this.firebaseService.registrarLog(mensaje);
     this.firebaseService.registrarLog('login App.. 02');
   }//loginFaceApp
 
