63b79e3d8e0e495c5fb9f7315d08a9e3e16af03b
DESKTOP-LPOB9EE\Leonardo <ivanlch08@gmail.com>
2018-04-14 22:24:50 -0500

    primera prueba con login a facebook desde app 05

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,10 +3,10 @@ import { NavController } from 'ionic-angular';
 import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
 //import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-//import { AngularFireList } from 'angularfire2/database';
+import { AngularFireList } from 'angularfire2/database';
 //import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import  firebase  from 'firebase';
-//import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -18,9 +18,8 @@ export class HomePage {
   newItem = '';
 
   userData = null;
-  //mensajeLog = null;
-  //constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider, private facebook:Facebook) {
-  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider) {
+  mensajeLog = '';
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider, private facebook:Facebook) {
     this.shoppingItems = this.firebaseService.getShoppingItems();
   }
 
@@ -38,23 +37,23 @@ export class HomePage {
     let provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithRedirect(provider).then(()=>{
       firebase.auth().getRedirectResult().then((result)=>{
-        //this.userData = { email: result['email'], first_name: result['first_name'], picture: result['picture_large']['data']['url'], username: result['name'] };
-        //this.mensajeLog = JSON.stringify(result);
-        //alert(JSON.stringify(result));
+        this.userData = { email: result['email'], first_name: result['first_name'], picture: result['picture_large']['data']['url'], username: result['name'] };
+        this.mensajeLog = JSON.stringify(result);
+        alert(JSON.stringify(result));
       }).catch(function(error){
-        //this.mensajeLog = JSON.stringify(error);
-        //alert(JSON.stringify(error));
+        this.mensajeLog = JSON.stringify(error);
+        alert(JSON.stringify(error));
       })
     })
   }//loginFaceWeb
 
-  /*loginFaceApp(){
+  loginFaceApp(){
     this.facebook.login(['email','public_profile']).then((response:FacebookLoginResponse) => {
       this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
         this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] };
       })
     });
   }//loginFaceApp
-*/
+
 }//clase
 
